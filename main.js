08e88fc073cdeadc1788f521ec99ca4cf8d74e19
HamzaJirah <hamzajirah@gmail.com>
2018-01-21 23:37:18 +0100

    created img click function

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.4;
 // const [current, imgs] = [ document.querySelector('#current'), document.querySelectorAll('.imgs img')];
-imgs.forEach(img =>img.addEventListener('click', e => (current.src = e.target.src)));
+imgs.forEach(img =>img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+  current.src = e.target.src;
+}
